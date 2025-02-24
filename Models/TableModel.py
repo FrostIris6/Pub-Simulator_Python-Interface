e65225c7660729ecb86d359334a968d34a3f0998
Charles <chang.zhai.9856@student.uu.se>
2025-02-24 17:16:05 +0100

    test

--- Models/TableModel.py
+++ Models/TableModel.py
@@ -3,3 +3,4 @@
 #VIP -> pay at the table, for normal customer 
 #Button on the table "Go to the bar" -> Log in as a Bartender and "Check Out that table" 
 #-> Log in as a customer and you're allowed to pay
+#push
